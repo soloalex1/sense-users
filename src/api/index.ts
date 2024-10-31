1730e75ebbfbab5d1cf7bcdfe54cc5504fe8ef83
Alexandre Machado <amch.smd@gmail.com>
2024-10-31 20:59:48 -0300

    feat(store): create redux store structure

---
+++ src/api/index.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { User } from '../types';
+
+export const api = createApi({
+  reducerPath: 'usersApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com',
+  }),
+  tagTypes: ['USER', 'DETAILS'],
+  endpoints: (builder) => ({
+    getUsers: builder.query<User[], void>({
+      query: () => '/users',
+      providesTags: ['USER'],
+    }),
+    getUserDetails: builder.query({
+      query: ({ id }) => ({ url: '/users', params: { id } }),
+      providesTags: ['DETAILS'],
+    }),
+  }),
+});
+
+export const { useGetUsersQuery, useGetUserDetailsQuery } = api;
