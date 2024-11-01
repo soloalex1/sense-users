455481014b1004d4092bb913120a327fc46ed77b
Alexandre Machado <amch.smd@gmail.com>
2024-10-31 21:24:17 -0300

    feat(search): debounce search value

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
 import { useGetUsersQuery } from '../api';
 
+import useDebounce from '../hooks/useDebounce';
+
 export default function HomePage() {
-  const { data, isLoading, isError } = useGetUsersQuery();
+  const [search, setSearch] = useState<string>('');
+
+  const debouncedSearch = useDebounce(search);
+
+  const { data, isLoading, isError } = useGetUsersQuery(debouncedSearch);
 
   return (
-    <div>
-      {data?.map((user) => (
-        <span key={user.id}>{user.name}</span>
-      ))}
-    </div>
+    <main>
+      <h1>Lista de usuários</h1>
+
+      <input
+        type="search"
+        name="search"
+        placeholder="Pesquise usuários por nome"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div>
+        {data?.map((user) => (
+          <span key={user.id}>{user.name}</span>
+        ))}
+      </div>
+    </main>
   );
 }
