c815d9f9e87a5c9b700f8c699afe7d8e2a0f6ecf
Alexandre Machado <amch.smd@gmail.com>
2024-10-31 22:14:14 -0300

    feat: add gfonts to project

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,10 +2,31 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  source: {
-    alias: {
-      '@/components': './src/components',
-    },
+  html: {
+    title: 'Lista de usuários',
+    tags: [
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          href: 'https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+          rel: 'stylesheet',
+        },
+      },
+    ],
   },
+  plugins: [pluginReact()],
 });
